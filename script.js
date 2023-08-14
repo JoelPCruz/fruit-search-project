47f48320759e67d6843e34e74d604bf60110e411
Joel Cruz <104873706+Joerican305@users.noreply.github.com>
2023-08-14 10:52:13 -0400

    built search functions

--- script.js
+++ script.js
@@ -3,26 +3,70 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
+// function search(str) {
+// 	let results = [];
+
+// 	// TODO
+
+// 	return results;
+// }
+const search = str => {
 	let results = [];
-
-	// TODO
-
+	results.push(fruit.filter(val => val.toLowerCase().includes(str)))
 	return results;
 }
 
-function searchHandler(e) {
-	// TODO
+
+// function searchHandler(e) {
+// 	// TODO
+// }
+// on key up see if key is included in array
+
+
+// search function will be nested inside
+const searchHandler = e => {
+	const inputVal = e.target.value.toLowerCase();
+	let results = [];
+	if(inputVal.length > 0) { // if there is an inputVal
+		results = search(inputVal) // run the search function on the val and add the results arr
+	}
+	showSuggestions(results, inputVal)
 }
 
-function showSuggestions(results, inputVal) {
+// function showSuggestions(results, inputVal) {
 
-	// TODO
-}
+// 	// TODO
+// }
 
-function useSuggestion(e) {
-	// TODO
-}
+// should create a list of each item that includes the str
+const showSuggestions = (results, inputVal) => {
+	suggestions.innerHTML = "";
+	if (results.length > 0) {
+		results.filter(val => {
+		// for each val in the array, list each fruit 
+		val.forEach(fruit => {
+			return suggestions.innerHTML += `
+				<li>${fruit}</li>
+			`;
+		});
+		});
+		}
+	}
+// function useSuggestion(e) {
+// 	// TODO
+// }
 
-input.addEventListener('keyup', searchHandler);
+const useSuggestion = e => 
+
+
 suggestions.addEventListener('click', useSuggestion);
+input.addEventListener('keyup', searchHandler);
+
+// every time a key is pressed
+	// run search handler/ takes the current input value.toLowerCase 
+		// run search with the current input value we just created
+		// filters through the arr.toLowerCase to see if the current input value
+		// Is Includinded inside the arr
+		// If it is included 
+			// run showSuggestions
+				// I need to list all the elements in the arr that include the input val
